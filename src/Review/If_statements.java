f6944a17cc14aee64f8577383fcc5843b073b00f
minseok_chey <techalt7@gmail.com>
2023-01-25 20:41:41 +0900

    "Review Day 1"

---
+++ src/Review/If_statements.java
@@ -0,0 +1,54 @@
+package Review;
+
+import java.util.Scanner;
+
+public class If_statements {
+    // Write a Java program to get a number from the user and print whether it is positive or negative.
+
+    public static void main(String[] args) {
+        /*
+        Scanner keyboard  = new Scanner(System.in);
+
+        int input = keyboard.nextInt();
+
+        if (input > 0) {
+            System.out.println("The number is positive");
+        }
+
+        else if (input < 0){
+            System.out.println("The number is negative");
+        }
+
+        else{
+            System.out.println("The number is zero");
+        }
+
+         */
+
+
+        // Write a program in Java to print the Floyd's Triangle
+
+        /*
+        Input number of rows: 5
+        1
+        2 3
+        4 5 6
+        7 8 9 10
+        11 12 13 14 15
+         */
+
+        //Scanner, println
+
+
+        Scanner keyboard  = new Scanner(System.in);
+        int input = keyboard.nextInt();
+
+        for (int i = 0; i < input; i++) {
+            for (int j = 0; j < i + 1 ; j++) {
+                
+            }
+        }
+
+
+    }
+}
