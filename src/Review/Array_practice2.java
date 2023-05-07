81c90ed38229b2187b82b4669e1215ff4498a5e3
minseok_chey <techalt7@gmail.com>
2023-05-07 11:58:57 +0900

    "Review for array"

--- src/Review/Array_practice2.java
+++ src/Review/Array_practice2.java
@@ -9,12 +9,39 @@ public class Array_practice2 {
 
          */
         int [] arrayTotal = new int[8];
-        int math[] = {10, 20, 30, 40, 50, 60, 70, 80};
+        char [] grade = new char [8];
+        int count = 0;
+        int math[] = {40, 20, 30, 40, 50, 46, 37, 25};
 
-        int Chem[] = {10, 20, 30, 40, 50, 60, 70, 80};
+        int Chem[] = {38, 47, 21, 43, 49, 36, 47, 39};
 
+
+
+        System.out.println("math" + " " + "chem" + " " + "total"+ " " + "grade");
         for (int i = 0; i < 8; i++) {
             arrayTotal[i] = math[i] + Chem[i];
+
+
+            if (arrayTotal[i] >= 90) {
+                grade[i] = 'A';
+                count++;
+            }
+            else if (arrayTotal[i] >= 80) {
+                grade[i] = 'B';
+
+            }
+            else if (arrayTotal[i] >= 70) {
+                grade[i] = 'C';
+            }
+            else if (arrayTotal[i] >= 60) {
+                grade[i] = 'D';
+            }
+            else {
+                grade[i] = 'F';
+            }
+
+            System.out.println(math[i] + "   " + Chem[i] + "   "  + arrayTotal[i] + "    " + grade[i]);
+
             /*
             arrayTotal[0] = math[0]+Chem[0]= 20
             arrayTotal[1] = 40
@@ -23,9 +50,9 @@ public class Array_practice2 {
         }
 
         /*
-                math    chem    total
-                10      10      20
-                20      20      40
+                math    chem    total   grade
+                10      10      20      F
+                20      20      40      F
          */
     }
 }
