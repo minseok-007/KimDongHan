31f7b6a1791db11fe7de0ff0e319c06f8e7830d7
minseok_chey <techalt7@gmail.com>
2023-05-10 21:29:18 +0900

    "Review for array"

--- src/Review/Array_practice3.java
+++ src/Review/Array_practice3.java
@@ -7,13 +7,17 @@ public class Array_practice3 {
         int arr1[] = {1,3,5,7, 8, 10, 12};
         int arr2[] = {1,2,3,6, 14,16, 20};
 
+        //1 2 3
+        //4 5 6
+
         int arr3[] = new int [arr1.length + arr2.length];
         // {1,1,2,3,5,6,7,8,
         int count = 0;
         int arr1Index = 0;
         int arr2Index = 0;
         while (count < arr1.length + arr2.length) {
-            if (arr1[arr1Index] >= arr2Index) {
+            if (arr1[arr1Index] >= arr2[arr2Index]) { // arr1[arr1Index]:  /    arr2[arr2Index]:
+                arr3[count] = arr2[arr2Index];
 
             }
         }
