876cc56744a1d21ff63ba269aa621a16bac0a0d9
minseok_chey <techalt7@gmail.com>
2023-05-21 22:36:16 +0900

    "Review for array"

--- src/Review/Array_practice4.java
+++ src/Review/Array_practice4.java
@@ -4,18 +4,26 @@ public class Array_practice4 {
 
     public static void main(String[] args) {
         // array of type int that stores 5 different heights
-        int height [] = {140, 150, 160, 170, 180};
+        double height [] = {140, 150, 160, 170, 180};
 
         // array of type double that stores 5 different weights
 
-        double weight [] = {40, 45, 50, 55, 60};
+        double weight [] = {40, 50, 80, 50, 110};
 
         // array of type double that stores bmi, using the value of array height and weight
         // bmi is calculated as weight/(height*height)
 
         double [] bmi = new double[5];
 
-        for (int i = 0; i >)
+        for (int i = 0; i < 5; i++){
+            bmi [i] =  weight[i] /  (height[i]/100 *height [i]/100);
 
+            System.out.println(bmi[i]);
+        }
+
+        // bmi 18.5 이하면 저체중, 18.5 - 22.9 사이면 정상, 23.0 - 24.9 사이면 과체중, 25이상부터는 비만
+        // Find the number of 저체중, 정상, 과체중, and 비만
+
+        for ()
     }
 }
