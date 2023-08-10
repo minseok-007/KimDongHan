c900780ae58aaa7405fd93a2874cd73afa1b8020
minseok_chey <techalt7@gmail.com>
2023-08-10 19:24:06 +0900

    "HW for Practice3"

--- src/Practice/Practice2.java
+++ src/Practice/Practice2.java
@@ -7,9 +7,27 @@ public class Practice2 {
         // Input two numbers with space 5 times (first number is A's number and second number is B's number).
         // Count each time whose number is higher, and output who has more score, thereby solving for final winner.
         // Final Output should look like: A won x times, and B won y times, so final Winner is A (or B).
+        Scanner scanner = new Scanner(System.in);
+        int aScore = 0;
+        int bScore = 0;
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter two numbers separated by a space: ");
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            if (a > b) {
+                aScore++;
+            } else if (b > a) {
+                bScore++;
+            }
+        }
+        if(aScore > bScore) {
+            System.out.println("A won" + aScore + "times, and B won" + bScore + "times so the final winner is A.");
+        } else if(bScore > aScore) {
+            System.out.println("B won" + bScore + "time, and A won" + aScore + "times so the final winner is B.");
 
-
-
+        } else {
+            System.out.println("A and B tied");
+        }
     }
 }
 
---
+++ src/Practice/Practice3.java
@@ -0,0 +1,38 @@
+package Practice;
+
+public class Practice3 {
+    // Find the sum of the values on the diagonal from [0][0] to [num_rows-1][num_rows-1] --> [2][2]
+
+    /*
+    1 2 3
+    4 5 6
+    7 8 9
+
+
+[0][0]
+[1][1]
+[2][2]
+     */
+    public static void main(String[] args) {
+        int[][] array = { {1,2,3},{-1,-2,-3},{4,5,6} };
+
+        int numrows = array.length;
+        int sum = 0;
+
+        for(int i = 0; i < numrows; i++){
+            for(int j = 0; j < numrows; j++){
+                if
+
+            }
+
+        }
+
+
+
+
+
+
+
+        
+    }
+}
